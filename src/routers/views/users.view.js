c3538ecbdc5b54c01d2194d6901ee17fa6cb8a39
Lautaro Hoffmann <lautyh77@gmail.com>
2024-04-23 14:06:15 -0300

    Solucion Views products

--- src/routers/views/users.view.js
+++ src/routers/views/users.view.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import userManager from "../../data/fs/UsersManager.fs.js";
 
 const usersRouter = Router();
+
+usersRouter.get("/", async (req, res, next) => {
+    try {
+      const users = await userManager.read();
+      return res.render("users", { title: "users", users });
+    } catch (error) {
+      return next(error);
+    }
+  });
 
 export default usersRouter;
