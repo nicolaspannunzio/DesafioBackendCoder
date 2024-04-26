9b1d2352a4fc80b3cda574c3779b7a012d8159c5
Lautaro Hoffmann <lautyh77@gmail.com>
2024-04-26 20:06:31 -0300

    Arreglos en products

--- src/routers/index.socket.js
+++ src/routers/index.socket.js
@@ -1,10 +1,21 @@
 import usersManager from "../data/fs/UsersManager.fs.js";
+import productsManager from "../data/fs/ProductsManager.fs.js"
 
 export default async (socket) => {
   console.log("client id: " + socket.id);
+
   socket.emit("users", await usersManager.read());
+
   socket.on("register", async (data) => {
     await usersManager.create(data);
     socket.emit("users", await usersManager.read());
-  });
+  })
+
+  socket.emit("products", await productsManager.read());
+
+  socket.on("newProduct", async (data) => {
+    await productsManager.create(data);
+    const updatedProducts = await productsManager.read()
+    socket.emit("products", updatedProducts);
+  })
 };
