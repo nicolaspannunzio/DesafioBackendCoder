793fe0840d90f9c040c851dbba560f16d0f799a1
Nicolas Pannunzio <nicolas.a.pannunzio@gmail.com>
2024-04-29 10:03:56 -0300

    Actualizaciones en rama users

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import "dotenv/config.js";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -10,11 +11,18 @@ import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
 import path from "path";
 import multer from "./src/middlewares/multer.mid.js";
+import dbConnect from "./src/utils/dbConnect.util.js";
+
+//console.log("todas las variables de entorno: " + process.env);
+//console.log(process.env.MONGO_URI);
 
 //server
 const server = express();
-const port = 8080;
-const ready = () => console.log("server ready on port " + port);
+const port = process.env.PORT || 9000;
+const ready = async () => {
+  console.log("server ready on port " + port);
+  await dbConnect();
+};
 const nodeServer = createServer(server);
 //server.listen(port, ready);
 nodeServer.listen(port, ready);
@@ -29,43 +37,36 @@ server.engine("handlebars", engine());
 server.set("view engine", "handlebars");
 server.set("views", __dirname + "/src/views");
 
-server.use(
-  "/css/bootstrap",
-  express.static(path.join(__dirname, "/public"))
-);
+server.use("/css/bootstrap", express.static(path.join(__dirname, "/public")));
 
-server.use(
-  "/js/bootstrap",
-  express.static(path.join(__dirname, "/public"))
-);
+server.use("/js/bootstrap", express.static(path.join(__dirname, "/public")));
 
 //middlewares
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(express.static(__dirname+"/public"))
-
+server.use(express.static(__dirname + "/public"));
 
 //router
 server.get("/home", async (requerimientos, respuesta) => {
-   try {
+  try {
     return respuesta.status(200).json({
       response: "Coder Api",
-       success: true,
-     });
-   } catch (error) {
-     console.log(error);
-     return respuesta.status(500).json({
-       response: "Coder Api Error",
-       success: false,
-     });
-   }
- });
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return respuesta.status(500).json({
+      response: "Coder Api Error",
+      success: false,
+    });
+  }
+});
 
 //image & form
- server.post("/multer.mid", multer.single("photo"), (req, res) => {
-   res.send(req.file.filename);
- });
+server.post("/multer.mid", multer.single("photo"), (req, res) => {
+  res.send(req.file.filename);
+});
 
 //? endpoints users and products
 server.use("/", indexRouter);
